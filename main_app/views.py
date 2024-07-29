71edf99473616ee84eb8bac1222bceafc1fbfa29
matthew-estes <matthew.estes19@gmail.com>
2024-07-29 09:16:13 -0400

    import user and add to cat model

--- main_app/views.py
+++ main_app/views.py
@@ -37,10 +37,21 @@ def cat_index(request):
 
 def cat_detail(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
+    toys_cat_doesnt_have = Toy.objects.exclude(id__in = cat.toys.all().values_list('id'))
     feeding_form = FeedingForm()
     return render(
-        request, "cats/detail.html", {"cat": cat, "feeding_form": feeding_form}
+        request, "cats/detail.html", {"toys": toys_cat_doesnt_have, "cat": cat, "feeding_form": feeding_form}
     )
+
+def associate_toy(request, cat_id, toy_id):
+    Cat.objects.get(id=cat_id).toys.add(toy_id)
+    return redirect('cat-detail', cat_id=cat_id)
+
+def remove_toy(request, cat_id, toy_id):
+    cat = Cat.objects.get(id=cat_id)
+    toy = Toy.objects.get(id=toy_id)
+    cat.toys.remove(toy)
+    return redirect('cat-detail', cat_id=cat.id)
 
 
 def add_feeding(request, cat_id):
@@ -72,3 +83,4 @@ class ToyUpdate(UpdateView):
 class ToyDelete(DeleteView):
     model = Toy
     success_url = '/toys/'
+
